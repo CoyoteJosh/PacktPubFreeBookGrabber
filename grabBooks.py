e3006b993ae57bc81d2b910d46536454df6293c8
Joshua Henry <joshua-henry@outlook.com>
2016-08-02 09:26:16 -0500

    locate file even if not executed from the same directory as .py file

--- grabBooks.py
+++ grabBooks.py
@@ -1,12 +1,17 @@
-import requests
-import lxml.html
-import re
-import json
 import base64
+import json
+import os
+import re
 
+import requests
+
+import lxml.html
+
+def find_file(file_name):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), file_name)
 
 def load_credentials(credentials_file):
-    with open(credentials_file) as file:
+    with open(find_file(credentials_file)) as file:
         credentials = json.loads(file.read())
         return base64.b64decode(credentials["username"]), base64.b64decode(credentials["password"])
 
@@ -26,15 +31,17 @@ with requests.session() as session:
             form = element
             break
 
-    login_payload = {form.inputs.keys()[index]: form.inputs[key].value for index, key in enumerate(form.inputs.keys())}
-    login_payload["email"], login_payload["password"] = load_credentials("credentials.json")
+    login_payload = {form.inputs.keys()[index]: form.inputs[
+        key].value for index, key in enumerate(form.inputs.keys())}
+    login_payload["email"], login_payload[
+        "password"] = load_credentials("credentials.json")
     login = session.post(free_book_url, data=login_payload, verify=False)
     print("Today's free eBook is {0}".format(get_title(login.text)))
 
     claim_search = re.search('\/freelearning-claim\/\d+\/\d+', login.text)
-    claim_book = session.get('https://www.packtpub.com' + claim_search.group(0), allow_redirects=False)
+    claim_book = session.get(
+        'https://www.packtpub.com' + claim_search.group(0), allow_redirects=False)
     if claim_book.status_code == 302:
         print("Successfully redeemed.")
     else:
         print("Error redeeming book. ")
-
